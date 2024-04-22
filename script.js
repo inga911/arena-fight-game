59ae81d27a25f44a3e9af9569d56e3b63e38e8fa
inga911 <inga.banaityte221@gmail.com>
2024-04-22 22:22:30 +0300

    final arena fight

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-
-
-
 const goldWon = document.getElementById('goldWon')
 const atackBtn = document.getElementById('atackBtn')
 const playerHP = document.getElementById('playerHP')
@@ -17,6 +13,13 @@ const startBtn = document.getElementById('gameStartBtn')
 const gameOverBtn = document.getElementById('gameOverBtn')
 const mainPlayerImg = document.getElementById('mainPlayerImg')
 const gameResult = document.getElementById('gameResult')
+const consoleResult = document.getElementById('consoleResult')
+///////////   SOUNDS
+const hoverSound = new Audio("./click-button-131479.mp3");
+const startGameSound = new Audio("./game-start-6104.mp3")
+const gameOverSound = new Audio("./failure-2-89169.mp3")
+const volume = document.getElementById('volume')
+
 ///////////    ENEMY IMAGES
 const images = [
     'https://www3.minijuegosgratis.com/v3/games/thumbnails/236711_7_sq.jpg',
@@ -33,8 +36,22 @@ const player5 = document.getElementById('player5')
 let allPlayers = []
 allPlayers.push(player1, player2, player3, player4, player5)
 
-let wapons = []
-wapons.push(sword, bow, magicWand)
+let weapons = []
+weapons.push(sword, bow, magicWand)
+
+let isMuted = false
+function muteUnmute() {
+    isMuted = !isMuted
+    hoverSound.muted = isMuted;
+    startGameSound.muted = isMuted;
+    gameOverSound.muted = isMuted;
+    if (isMuted) {
+        volume.innerHTML = `<span style="font-size:35px;" class="material-symbols-outlined">volume_off</span>`;
+    } else {
+        volume.innerHTML = `<span  style="font-size:35px;" class="material-symbols-outlined">volume_up</span>`;
+    }
+}
+volume.addEventListener('click', muteUnmute);
 
 
 /////////     CLASSES AND STYLES
@@ -44,6 +61,7 @@ enemyHP.style.backgroundColor = 'lightgreen';
 main.classList.add('d-none');
 gameOver.classList.add('d-none');
 sword.style.border = ' 2px solid red'
+consoleResult.classList.add('d-none')
 
 //////////     BUTTONS
 startBtn.onclick = () => {
@@ -51,7 +69,6 @@ startBtn.onclick = () => {
         checkPlayerClicked();
         return;
     }
-
     startGame.classList.add('d-none');
     main.classList.remove('d-none');
     setRandomEnemyImage();
@@ -76,11 +93,17 @@ function handlePlayerClick(clickedImage) {
     clickedImage.style.border = '3px solid #4666FF';
     previousClickedImage = clickedImage;
     playerClicked = true;
-
 }
 
 allPlayers.forEach(player => {
     player.onclick = () => handlePlayerClick(player);
+    player.addEventListener('mouseover', () => {
+        sound(hoverSound)
+    });
+});
+
+startBtn.addEventListener('mouseover', () => {
+    sound(startGameSound)
 });
 
 function checkPlayerClicked() {
@@ -105,9 +128,11 @@ function handleWeaponClick(weapon) {
     weapon.style.border = '2px solid red';
     weapon.style.scale = '1.2';
     previousClickedWeapon = weapon;
+    hoverSound.currentTime = 0;
+    sound(hoverSound)
 }
 
-wapons.forEach(weapon => {
+weapons.forEach(weapon => {
     weapon.addEventListener('click', () => handleWeaponClick(weapon));
 });
 
@@ -116,6 +141,7 @@ wapons.forEach(weapon => {
 let gold = 0;
 let playerHpBar = 100;
 let enemyHpBar = 100;
+let imageChangeCount = 0;
 
 const random = (num) => Math.floor(Math.random() * num + 1);
 
@@ -124,11 +150,11 @@ function textContentResult() {
     resultEnemy.textContent = enemyHpBar + ' / 100%';
 }
 
-
 function setRandomEnemyImage() {
     const randomIndex = Math.floor(Math.random() * images.length);
     const randomEnemyImage = images[randomIndex];
     enemyImg.style.backgroundImage = `url(${randomEnemyImage})`;
+    imageChangeCount++
 }
 
 const addRemoveDisable = () => {
@@ -146,20 +172,19 @@ function updateHtmlPlayer() {
 }
 
 atackBtn.onclick = () => {
+    sound(hoverSound)
     const playerDamage = random(12);
     let enemyDamage = random(8);
     const percentage = random(100)
     if (previousClickedWeapon === sword && percentage < 25) {
         enemyDamage = 0;
+        playerHpBar += enemyDamage
     }
     if (previousClickedWeapon === bow && percentage < 35) {
-        const doublePlayerDamage = playerDamage;
-        enemyHpBar -= doublePlayerDamage;
+        enemyHpBar -= playerDamage;
     }
-    if (previousClickedWeapon === magicWand && percentage < 20) {
-        updateHealthBar(playerHP, playerHpBar);
-        updateHtmlPlayer();
-        enemyDamage = 0;
+    if (previousClickedWeapon === magicWand && percentage < 40) {
+        playerHpBar += random(8)
     }
     const randomGold = random(5);
     gold += randomGold;
@@ -173,14 +198,14 @@ atackBtn.onclick = () => {
 
     if (enemyHpBar <= 0) {
         setRandomEnemyImage();
-        enemyHpBar = 100;
+        enemyHpBar = 100
     }
     addRemoveDisable();
     textContentResult();
     updateHealthBar(playerHP, playerHpBar);
     updateHealthBar(enemyHP, enemyHpBar);
+    showConsole(playerDamage, enemyDamage, imageChangeCount, previousClickedWeapon);
 };
-
 
 potion.onclick = () => {
     if (gold >= 50) {
@@ -188,6 +213,7 @@ potion.onclick = () => {
         goldWon.textContent = gold;
         updateHtmlPlayer()
         resultPlayer.textContent = playerHpBar
+        sound(hoverSound)
     }
 }
 
@@ -203,6 +229,7 @@ function updateHealthBar(hp, hpBar) {
         hp.style.backgroundColor = 'white'
     }
 }
+
 function resetGameData() {
     gold = 0;
     playerHpBar = 100;
@@ -213,8 +240,11 @@ function resetGameData() {
     updateHealthBar(enemyHP, enemyHpBar);
 }
 
-
 function resetGame() {
+    if (playerHpBar <= 0) {
+        resetGameData()
+    }
+    sound(gameOverSound)
     resetGameData();
     main.classList.add('d-none');
     gameOver.classList.remove('d-none');
@@ -225,3 +255,23 @@ function resetGame() {
     }
 }
 
+function sound(soundType) {
+    soundType.currentTime = 0;
+    soundType.play();
+    soundType.volume = 0.5
+}
+
+
+function showConsole(playerDamage2, enemyDamage2, round) {
+    const playerDamageInText = playerDamage2;
+    const enemyDamageInText = enemyDamage2;
+    const selectedWeaponId = previousClickedWeapon.id;
+    const logEntry = `<div style="margin-bottom: 10px;">ROUND: ${round} <br>
+    Player damage:   ${playerDamageInText}<br>
+      Enemy damage:  ${enemyDamageInText}<br>
+      Player Hp Left: ${playerHpBar} <br>
+      Monster Hp Left: ${enemyHpBar}  <br>
+      Player weapon: ${selectedWeaponId}  <br> </div>`;
+    consoleResult.insertAdjacentHTML('afterbegin', logEntry);
+    consoleResult.classList.remove('d-none');
+}
